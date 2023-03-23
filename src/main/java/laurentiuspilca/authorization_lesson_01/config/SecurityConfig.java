98b2c3f2c1d7b3673bbdfc7dbb2901a50cb2aced
mrtylmz <mrtylmz5306@gmail.com>
2023-03-23 11:28:08 +0300

    Some security options updated second time

--- src/main/java/laurentiuspilca/authorization_lesson_01/config/SecurityConfig.java
+++ src/main/java/laurentiuspilca/authorization_lesson_01/config/SecurityConfig.java
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 public class SecurityConfig {
@@ -21,15 +19,17 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests()
                 //.authorizeRequests() // DEPRECATED WAY BUT LAURENTİU SPİLCA'S WAY
-                // .requestMatchers("/demo/**") DEMO UZANTILI END POİNT'E GELEN BÜTÜN REQUESTLERİ KAPSAR
+                // .requestMatchers("/demo/**") //DEMO UZANTILI END POİNT'E GELEN BÜTÜN REQUESTLERİ KAPSAR
                 //.anyRequest()//MATCHER METHOD (BÜTÜN GELEN İSTEKLER)
-                //.anyRequest().hasAuthority("ADMIN") AUTHORİZATİON RULE (SADECE ADMIN OLANLAR)
-                //.anyRequest().permitAll() HEPSİNE İZİN VERİR
-                //.anyRequest().denyAll() HEPSİNİ REDDEDER
-                //.anyRequest().hasRole("ADMIN") (hasAuthority() ile eşdeğerdir)
-                //.anyRequest().hasAnyRole("ADMIN","USER") ()
-
-                .anyRequest().access(new WebExpressionAuthorizationManager("isAuthenticated() and hasAuthority('ADMIN') "))//DAHA KARIŞIK LOCİGLER YAZMAK İÇİN
+                //.anyRequest().hasAuthority("ADMIN") //AUTHORİZATİON RULE (SADECE ADMIN OLANLAR)
+                //.anyRequest().permitAll()///HEPSİNE İZİN VERİR
+                //.anyRequest().denyAll() //HEPSİNİ REDDEDER
+                //.anyRequest().hasRole("ADMIN")// (hasAuthority() ile eşdeğerdir)
+                //.anyRequest().hasAnyRole("ADMIN","USER")
+                //.anyRequest().access(new WebExpressionAuthorizationManager("isAuthenticated() and hasAuthority('ADMIN') "))//DAHA KARIŞIK LOCİGLER YAZMAK İÇİN
+                //.requestMatchers(HttpMethod.GET,"/demo/**").hasAuthority("ADMIN").anyRequest().authenticated() //DEMO END POİNT'DEN ALINAN GET METODLARINDA ADMIN'LİK İSTER, GERİ KALAN HERŞEYDE AUTHANTİCATED OLMAK YETERLİ
+                .anyRequest().authenticated()
+                //.and().csrf().disable().build(); // NEVER DON'T DO THİS IN REAL WORLD PROJECTS(POSTMANDAN POST ATABİLMEK İÇİN GEREKLİ)
                 .and().build();
     }
 
